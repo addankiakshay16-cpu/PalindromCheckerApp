65570f044899c0f5b2fe3e314dfcd6dbcc021c4d
addankiakshay16-cpu <addankiakshay16@gmail.com>
2026-03-09 15:08:30 +0530

    uc2 done

--- app/src/PalandromCheckerApp.java
+++ app/src/PalandromCheckerApp.java
@@ -1,7 +1,14 @@
-public class PalandromCheckerApp {
-    static void main(String[] args) {
-        System.out.println("welcome to palidrome checker management system");
-        System.out.println("version : 1.0" );
-        System.out.println("system initialized successfully");
+public class PalindromeCheckerApp {
+    public static void main(String args[]){
+        String str="madam";
+        boolean ispalindrome=true;
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length() -1 -i)){
+                ispalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input text: "+str);
+        System.out.println("\n Is it a palindrome : True");
     }
 }
